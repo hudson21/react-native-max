66a7399ccc9ffb6a4c1b98f1ce0108ccf577f470
hudson21 <camihudsondiaz@ittepic.edu.mx>
2020-05-04 21:05:08 -0500

    4.17 Adding Random Number Generation

--- guess-number-app/screens/StartGameScreen.js
+++ guess-number-app/screens/StartGameScreen.js
@@ -12,6 +12,7 @@ import {
 // Components
 import Card from "../components/Card";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 
 // Constans
 import Colors from "../constants/colors";
@@ -43,12 +44,19 @@ const StartGameScreen = () => {
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue("");
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You Selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME" />
+      </Card>
+    );
   }
 
   return (
@@ -121,6 +129,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: "center",
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: "center",
   },
 });
 
